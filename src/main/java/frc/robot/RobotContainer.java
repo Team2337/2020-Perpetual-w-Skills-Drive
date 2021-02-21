dabd25bc0ff5b5a301041bc2001d323ac3c47425
Sean Lynch <slynch05@yahoo.com>
2021-02-21 15:41:21 -0500

    Updated ResetOdometry for PascallCase.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.resetOdometry;
+import frc.robot.commands.ResetOdometry;
 import frc.robot.commands.auto.LPathTrajectory;
 import frc.robot.commands.auto.Calibration.StraightLineTest10Ft;
 import frc.robot.commands.auto.Calibration.StraightLineTest10Ft0;
@@ -104,7 +104,7 @@ public class RobotContainer {
     final JoystickButton redB = new JoystickButton(controller, 2);
 
     greenA.whenPressed(() -> swerveDrivetrain.resetDriveEncoders());
-    redB.whenPressed(new resetOdometry(swerveDrivetrain));
+    redB.whenPressed(new ResetOdometry(swerveDrivetrain));
 
     SmartDashboard.putData("AutonChooser", autonChooser);
   }
--- src/main/java/frc/robot/commands/resetOdometry.java
+++ src/main/java/frc/robot/commands/resetOdometry.java
@@ -3,10 +3,10 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.SwerveDrivetrain;
 
-public class resetOdometry extends InstantCommand {
+public class ResetOdometry extends InstantCommand {
     private final SwerveDrivetrain drivetrain;
 
-    public resetOdometry(SwerveDrivetrain drivetrain) {
+    public ResetOdometry(SwerveDrivetrain drivetrain) {
         this.drivetrain = drivetrain;
         addRequirements(drivetrain);
     }
